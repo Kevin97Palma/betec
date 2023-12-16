252679be96f6321a62bdb505652ef159b71bbb25
Kevin97Palma <67132452+Kevin97Palma@users.noreply.github.com>
2023-12-15 23:07:44 -0500

    agrege menu de filtros funcinando

--- admin-buro/api/modulo-solicitudes/solicitudes.php
+++ admin-buro/api/modulo-solicitudes/solicitudes.php
@@ -3,16 +3,29 @@ include '../co/conexion.php';
 include '../co/config.php';
 session_start();
 
+// Inicializa el parámetro de estado a 1 si no se proporciona
+$estadoId = isset($_GET['estado']) ? $_GET['estado'] : 1;
 
-    // Consulta SQL para obtener los registros del menú
-    $sql = $pdo->query("SELECT dp.id, dp.cedula, dp.nombre, dp.celular, dp.correo, e.nombre_estado AS estado
+// Construye la consulta SQL base
+$sqlQuery = "SELECT dp.id, dp.cedula, dp.nombre, dp.celular, dp.correo, e.nombre_estado AS estado
     FROM datos_personales dp
-    JOIN estados e ON dp.estado_id = e.id;");
+    JOIN estados e ON dp.estado_id = e.id ";
 
-    if (!$sql) {
-        die('Error en la consulta SQL: ' . $pdo->errorInfo()[2]);
-    }
+// Agrega la condición WHERE si se proporciona un estado diferente de 3
+if ($estadoId != 3) {
+    $sqlQuery .= "WHERE e.id = :estado";
+}
 
+// Prepara la consulta SQL
+$sql = $pdo->prepare($sqlQuery);
+
+// Vincula el valor del parámetro de estado si se proporciona
+if ($estadoId != 3) {
+    $sql->bindParam(':estado', $estadoId, PDO::PARAM_INT);
+}
+
+// Ejecuta la consulta
+if ($sql->execute()) {
     // Recuperar todos los registros de la consulta
     $registros = $sql->fetchAll(PDO::FETCH_ASSOC);
 
@@ -23,5 +36,9 @@ session_start();
     ];
 
     echo json_encode($response);
-
+} else {
+    // Si hay un error en la consulta, devuelve un mensaje de error
+    http_response_code(500);
+    echo json_encode(["message" => "Error en la consulta SQL"]);
+}
 ?>
--- admin-buro/js/registros.js
+++ admin-buro/js/registros.js
@@ -1,22 +1,55 @@
 $(document).ready(function() {
+    // Llama a tu API inicialmente sin parámetros
+    cargarDatos();
+
+    // Agrega un evento de clic a los elementos del menú
+    $('#e1, #e2, #e3').on('click', function(e) {
+        // Previene el comportamiento predeterminado del enlace
+        e.preventDefault();
+        
+        // Obtiene el ID del elemento clicado
+        var estadoId = $(this).attr('id').substring(1);
+
+        // Llama a la función para cargar los datos con el parámetro de estado
+        cargarDatos(estadoId);
+    });
+});
+
+// Función para cargar los datos desde la API con un parámetro opcional de estado
+function cargarDatos(estadoId) {
+    // Construye la URL de la API con o sin el parámetro de estado
+    var apiUrl = estadoId ? `./api/modulo-solicitudes/solicitudes.php?estado=${estadoId}` : './api/modulo-solicitudes/solicitudes.php';
+
+    // Función para determinar el color de la alerta según el estado
+    function obtenerColorAlerta(estado) {
+        return estado === 'En Proceso' ? 'success' : (estado === 'Enviado' ? 'danger' : 'info');
+    }
+
+    // Muestra la solicitud que se enviará a la API
+    console.log(`Solicitud a la API: ${apiUrl}`);
+
     // Llama a tu API para obtener los datos
-    fetch('./api/modulo-solicitudes/solicitudes.php', {
+    fetch(apiUrl, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         },
     })
-    .then(response => response.json())
+    .then(response => {
+        // Muestra la respuesta de la API
+        console.log('Respuesta de la API:', response);
+        return response.json();
+    })
     .then(data => {
+        // Muestra los datos devueltos por la API
+        console.log('Datos recibidos:', data);
+
         // Manipula los datos y llena la tabla
         if (data.registros && data.registros.length > 0) {
             var table = $('#datatable-buttons').DataTable();
             table.clear().draw(); // Limpia la tabla antes de agregar nuevos datos
 
             data.registros.forEach(registro => {
-                // Determina el color del alert según el estado
-                var alertColor = registro.estado === 'En Proceso' ? 'success' : 'danger';
-
                 // Agrega cada registro como una fila en la tabla
                 table.row.add([
                     registro.id,
@@ -25,7 +58,7 @@ $(document).ready(function() {
                     registro.celular,
                     registro.correo,
                     `
-                        <p class="alert alert-${alertColor}" role="alert">${registro.estado}</p> <!-- Aquí muestra el estado -->
+                        <p class="alert alert-${obtenerColorAlerta(registro.estado)}" role="alert">${registro.estado}</p>
                     `,
                     // Agrega los botones en la nueva columna
                     `<button type="button" class="btn btn-primary" onclick="abrirModal(${registro.id})">Editar</button>
@@ -35,19 +68,7 @@ $(document).ready(function() {
         }
     })
     .catch(error => {
-        // Maneja errores
-        console.error(error);
+        // Muestra errores en la consola
+        console.error('Error al obtener datos:', error);
     });
-});
-
-// Función para abrir un modal
-function abrirModal(id) {
-    // Implementa la lógica para abrir el modal según el ID
-    console.log(`Abrir modal para el ID ${id}`);
-}
-
-// Función para eliminar un registro
-function eliminarRegistro(id) {
-    // Implementa la lógica para eliminar el registro según el ID
-    console.log(`Eliminar registro con ID ${id}`);
 }
