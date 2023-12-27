8be1b6b49d617810e8f45cf0886c2941cea1e803
Kevin97Palma <67132452+Kevin97Palma@users.noreply.github.com>
2023-12-27 11:38:25 -0500

    modificacion en el envio se guarda ya en la base

--- admin-buro/api/modal/enviar.php
+++ admin-buro/api/modal/enviar.php
@@ -3,43 +3,85 @@ include '../co/conexion.php';
 include '../co/config.php';
 session_start();
 
-$idR = $_GET['idR'] ; 
-$archivoAdjunto = base64_encode(file_get_contents($_FILES["buro"]["tmp_name"])); // Obtiene el archivo en base64
-// Construye la consulta SQL base
+$idR = $_POST['idR'];
+
+// Verificar si se recibió un archivo buro
+if (isset($_FILES['buro']) && $_FILES['buro']['error'] == UPLOAD_ERR_OK) {
+    // Obtener el contenido del archivo buro en base64
+    $archivoAdjunto = base64_encode(file_get_contents($_FILES['buro']['tmp_name']));
+} else {
+    // Si no se recibió un archivo buro, establecer el valor a NULL o manejarlo según tus necesidades
+    $archivoAdjunto = null;
+}
+
+// Construir la consulta SQL base
 $sqlQuery = "SELECT 
-d.id,
-d.cedula , 
-d.nombre,
-d.celular,
-d.correo,
-d.created_at, 
-d.codigoTr,
-d.banco, 
-e.nombre_estado,
-a.ruta_cedula_anverso,
-a.ruta_cedula_reverso,
-a.ruta_comprobante_pago
-FROM datos_personales d
-JOIN archivos a on d.id = a.cedula_id 
-JOIN estados e ON d.estado_id = e.id where d.id = $id  ";
+    d.id,
+    d.cedula, 
+    d.nombre,
+    d.celular,
+    d.correo,
+    d.created_at, 
+    d.codigoTr,
+    d.banco, 
+    e.nombre_estado,
+    a.ruta_cedula_anverso,
+    a.ruta_cedula_reverso,
+    a.ruta_comprobante_pago
+    FROM datos_personales d
+    JOIN archivos a ON d.id = a.cedula_id 
+    JOIN estados e ON d.estado_id = e.id 
+    WHERE d.id = :idR";
 
-
-// Prepara la consulta SQL
+// Preparar la consulta SQL
 $sql = $pdo->prepare($sqlQuery);
 
+// Asignar el valor a los parámetros de la consulta
+$sql->bindParam(':idR', $idR, PDO::PARAM_INT);
 
-// Ejecuta la consulta
+// Ejecutar la consulta
 if ($sql->execute()) {
-    // Recuperar todos los registros de la consulta
-    $registros = $sql->fetchAll(PDO::FETCH_ASSOC);
+    // Recuperar el primer registro de la consulta (asumiendo que solo hay uno)
+    $registro = $sql->fetch(PDO::FETCH_ASSOC);
 
-    // Devolver la información de la sesión y los registros del menú
-    $response = [
-        "id" => $_SESSION["id"],
-        "registros" => $registros,
-    ];
+    // Asignar los valores a variables individuales
+    $id = $registro['id'];
 
-    echo json_encode($response);
+    // Imprimir valores para depuración
+    echo "idR recibido: $idR\n";
+    echo "id del registro obtenido: $id\n";
+
+    // Actualizar el campo "ruta_comprobante_pago" en la tabla "archivos" con el archivo buro
+    $sqlUpdate = "UPDATE archivos 
+                  SET buro = :archivoAdjunto, 
+                      updated_at = NOW() 
+                  WHERE cedula_id = :idR";
+
+    // Preparar la consulta SQL para la actualización
+    $updateStatement = $pdo->prepare($sqlUpdate);
+
+    // Asignar los valores a los parámetros de la consulta
+    $updateStatement->bindParam(':archivoAdjunto', $archivoAdjunto, PDO::PARAM_STR);
+    $updateStatement->bindParam(':idR', $idR, PDO::PARAM_INT);
+
+    // Imprimir valores para depuración
+    echo "archivoAdjunto: $archivoAdjunto\n";
+
+    // Ejecutar la actualización
+    if ($updateStatement->execute()) {
+        // Devolver la información
+        $response = [
+            "id" => $_SESSION["id"],
+            "idRegistro" => $id,
+            // Agregar otras variables necesarias para la respuesta
+        ];
+
+        echo json_encode($response);
+    } else {
+        // Si hay un error en la actualización, devuelve un mensaje de error
+        http_response_code(500);
+        echo json_encode(["message" => "Error en la actualización SQL"]);
+    }
 } else {
     // Si hay un error en la consulta, devuelve un mensaje de error
     http_response_code(500);
--- admin-buro/js/envio.js
+++ admin-buro/js/envio.js
@@ -1,44 +1,59 @@
-$(".envio").click(function() {
-      console.log("holi boli");
+$(".envio").click(function () {
+    // Obtener el valor del campo idR
     var idR = $("#idR").val();
-  
+    // Obtener el archivo seleccionado en el campo buro
     var buro = $("#buro")[0].files[0];
 
+    // Crear un objeto FormData para enviar datos binarios (como archivos)
     var formData = new FormData();
+    // Agregar el valor de idR al FormData
     formData.append("idR", idR);
-    
-    if (firmaA) {
+
+    // Si hay un archivo seleccionado, agregarlo al FormData con la clave "buro"
+    if (buro) {
         formData.append("buro", buro);
     }
 
-    $.ajax({
-        url: "api/enviar.php",
-        type: "post",
-        data: formData,
-        contentType: false,
-        processData: false
-    }).then(function(data) {
-        console.log(data);
-        if (data == "2") {
-            swal({
-                title: "Datos guardados con éxito",
-                type: "success"
-            });
-            $("#modalIngresar1").modal("hide");
-            $(".table-responsive").load("getProveedores.php");
-        } else if (data == "3") {
-            swal({
-                title: "Datos actualizados con éxito",
-                type: "success"
-            });
-            $("#modalIngresar1").modal("hide");
-            $(".table-responsive").load("getProveedores.php");
-        } else if (data == "1") {
-            alert(data);
-            swal({
-                title: "Error. Consulte con el proveedor",
-                type: "error"
-            });
-        }
-    });
-   })
+    // Imprimir el archivo en base64 y otros datos por consola
+    for (var pair of formData.entries()) {
+        console.log(pair[0] + ': ' + pair[1]);
+    }
+
+    // Realizar una solicitud fetch a la URL "api/enviar.php" con el método POST y el cuerpo FormData
+    fetch("./api/modal/enviar.php", {
+        method: "POST",
+        body: formData,
+    })
+        .then(response => response.text())  // Convertir la respuesta a texto
+        .then(data => {
+            console.log(data);
+
+            // Manejar la respuesta según los valores devueltos por el servidor
+            if (data === "2") {
+                swal({
+                    title: "Datos guardados con éxito",
+                    type: "success",
+                });
+                // Ocultar el modal y recargar la tabla de proveedores
+                $("#modalIngresar1").modal("hide");
+                $(".table-responsive").load("getProveedores.php");
+            } else if (data === "3") {
+                swal({
+                    title: "Datos actualizados con éxito",
+                    type: "success",
+                });
+                // Ocultar el modal y recargar la tabla de proveedores
+                $("#modalIngresar1").modal("hide");
+                $(".table-responsive").load("getProveedores.php");
+            } else if (data === "1") {
+                alert(data);
+                swal({
+                    title: "Error. Consulte con el proveedor",
+                    type: "error",
+                });
+            }
+        })
+        .catch(error => {
+            console.error("Error en la solicitud:", error);
+        });
+});
